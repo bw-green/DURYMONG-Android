0785dc69acf8606ef30a4712da941e9472addd84
casperjr <hyunjw0515@gmail.com>
2025-01-07 01:56:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+DURYMONG Android
---
+++ app/src/main/java/com/example/durymong/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.durymong
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.durymong.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
