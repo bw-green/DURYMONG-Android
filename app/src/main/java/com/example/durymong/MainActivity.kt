b1ca122cd05eba768caf829301080107ba7b9b7b
casperjr <hyunjw0515@gmail.com>
2025-01-07 14:05:26 +0900

    feat: bottom navigation을 위해 MainActivity 작성(#2)

--- app/src/main/java/com/example/durymong/MainActivity.kt
+++ app/src/main/java/com/example/durymong/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.durymong
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.durymong.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        showBottomNavigation()
+    }
+
+    private fun showBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvMain.setupWithNavController(navController)
+        setBottomNavVisible(navController)
+    }
+
+    private fun setBottomNavVisible(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvMain.visibility = when (destination.id) {
+                R.id.mongFragment, R.id.chatFragment, R.id.columnFragment, R.id.doItFragment -> View.VISIBLE
+                else -> View.GONE
+            }
+        }
     }
 }
