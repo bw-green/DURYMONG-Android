2d209ef933deac28c55a397871184543ae50f801
bw-green <102360569+bw-green@users.noreply.github.com>
2025-01-14 11:43:54 +0900

    ui: do_it 5-5 각종 테스트 ui 작업 화면 연결 안함(#4)

---
+++ app/src/main/java/com/example/durymong/view/do_it/DoItPostTraumaStressTestFragment.kt
@@ -0,0 +1,20 @@
+package com.example.durymong.view.do_it
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.durymong.R
+
+class DoItPostTraumaStressTestFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_do_it_post_trauma_stress_test, container, false)
+    }
+
+}
