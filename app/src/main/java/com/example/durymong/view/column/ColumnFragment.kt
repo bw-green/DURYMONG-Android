6d4a49065c560352db7cc2185b3afa43f09aeee9
casperjr <hyunjw0515@gmail.com>
2025-01-22 17:58:00 +0900

    [feat]: column메뉴에서의 viewModel 초기 설정

--- app/src/main/java/com/example/durymong/view/column/ColumnFragment.kt
+++ app/src/main/java/com/example/durymong/view/column/ColumnFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.durymong.databinding.FragmentColumnBinding
+import com.example.durymong.view.column.viewmodel.ColumnViewModel
 
 class ColumnFragment : Fragment() {
     private var _binding: FragmentColumnBinding? = null
     private val binding get() = _binding!!
+
+    //viewModel 선언
+    private val viewModel: ColumnViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
