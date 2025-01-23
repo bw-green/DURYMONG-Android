548bf5b2a0f5c852eb97dfea58e397990bb00972
casperjr <hyunjw0515@gmail.com>
2025-01-23 12:53:46 +0900

    [feat]: 카테고리 클릭시 화면이동

--- app/src/main/java/com/example/durymong/view/column/ColumnFragment.kt
+++ app/src/main/java/com/example/durymong/view/column/ColumnFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.durymong.R
 import com.example.durymong.databinding.FragmentColumnBinding
 import com.example.durymong.view.column.adapter.RVAdapterColumnCategory
 import com.example.durymong.view.column.viewmodel.ColumnViewModel
@@ -39,7 +41,11 @@ class ColumnFragment : Fragment() {
     }
 
     private fun initRVAdapterColumnCategory(){
-        rvAdapterColumnCategory = RVAdapterColumnCategory(requireContext(), viewModel.columnCategoryList)
+        rvAdapterColumnCategory = RVAdapterColumnCategory(requireContext(), viewModel.columnCategoryList){
+            //이후에 api 연결시에 세부 내용은 아마도 변경될 예정
+            viewModel.fetchColumnData()
+            findNavController().navigate(R.id.action_fragment_column_to_fragment_column_detail)
+        }
 
         with(binding.rvColumnMenuList){
             adapter = rvAdapterColumnCategory
