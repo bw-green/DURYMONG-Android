8d050257c63fcb117b57bd522b007c140ea421b1
casperjr <hyunjw0515@gmail.com>
2025-01-23 15:04:23 +0900

    [feat]: 칼럼 카테고리의 물음표 클릭시 팝업 구현

--- app/src/main/java/com/example/durymong/util/Extensions.kt
+++ app/src/main/java/com/example/durymong/util/Extensions.kt
@@ -1,8 +1,10 @@
 package com.example.durymong.util
 
+import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Parcelable
+import kotlin.math.roundToInt
 
 //Parcelable 객체 전달을 위한 Bundle 확장 함수
 inline fun <reified T : Parcelable> Bundle.getSafeParcelable(key: String): T? =
@@ -10,3 +12,9 @@ inline fun <reified T : Parcelable> Bundle.getSafeParcelable(key: String): T? =
         SDK_INT >= 33 -> getParcelable(key, T::class.java)
         else -> @Suppress("DEPRECATION") getParcelable(key) as? T
     }
+
+//DP를 PX로 변환
+fun Int.dpToPx(context: Context): Int {
+    val density = context.resources.displayMetrics.density
+    return (this * density).roundToInt()
+}
