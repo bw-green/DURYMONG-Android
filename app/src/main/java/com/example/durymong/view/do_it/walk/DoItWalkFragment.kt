b1250c3dc2c3caf3282a26dea03e27397d3e2e05
bw-green <102360569+bw-green@users.noreply.github.com>
2025-01-31 09:43:03 +0900

    타이머 쪽인데 잘 안 돌아 가서 수정 예정

--- app/src/main/java/com/example/durymong/view/do_it/walk/DoItWalkFragment.kt
+++ app/src/main/java/com/example/durymong/view/do_it/walk/DoItWalkFragment.kt
@@ -2,20 +2,32 @@ package com.example.durymong.view.do_it.walk
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
 import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import com.example.durymong.databinding.FragmentDoItWalkBinding
+import com.example.durymong.view.do_it.walk.viewmodel.WalkViewModel
 
 
-class DoItWalkFragment : Fragment() {
+interface DialogCloseListener {
+    fun onDialogClosed(result: String)
+
+}
+
+class DoItWalkFragment : Fragment(), DialogCloseListener {
+
     private var _binding: FragmentDoItWalkBinding? = null
     private val binding get() = _binding!!
+
     private lateinit var circularProgressBar: ProgressBar
 
+    private val viewModel : WalkViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +35,32 @@ class DoItWalkFragment : Fragment() {
     ): View {
         _binding = FragmentDoItWalkBinding.inflate(layoutInflater)
 
-        initTimer()
 
+
+        initTimer()
+        initTimerSetting()
+
+        Log.d("onCreateView", "onCreateView")
         return binding.root
+    }
+    override fun onResume() {
+        viewModel.timeData.observe(viewLifecycleOwner) {
+            binding.tvDoItWalkTime.text = "${it.first}시간 ${it.second}분"
+        }
+        Log.d("onResume", "onResume")
+        super.onResume()
+    }
+
+    private fun initTimerSetting() {
+        val bottomSheet = TimerBottomSheet()
+        _binding?.ivDoItWalkDownArrow?.setOnClickListener {
+            bottomSheet.show(parentFragmentManager, bottomSheet.tag)
+            viewModel.isDialogOpen.observe(viewLifecycleOwner) { isOpen ->
+                if (!isOpen) {
+                    println("다이얼로그가 닫힘을 감지")
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -41,6 +76,10 @@ class DoItWalkFragment : Fragment() {
         animator.interpolator = LinearInterpolator() // 일정한 속도
         animator.start()
 
+    }
+
+    override fun onDialogClosed(result: String) {
+        Toast.makeText(requireContext(), result, Toast.LENGTH_SHORT).show()
     }
 
 }
---
+++ app/src/main/java/com/example/durymong/view/do_it/walk/TimerBottomSheet.kt
@@ -0,0 +1,135 @@
+package com.example.durymong.view.do_it.walk
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.NumberPicker
+import androidx.collection.IntIntPair
+import com.example.durymong.R
+import com.example.durymong.databinding.BottomSheetTimerBinding
+import com.example.durymong.view.do_it.walk.viewmodel.WalkViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlin.math.log
+
+class TimerBottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: BottomSheetTimerBinding? = null
+    private val binding get() = _binding!!
+
+    private var hour =0
+    private var minute =0
+
+    private val viewModel = WalkViewModel()
+
+
+    // 밑에 네비게이션바가 안보임
+//    override fun onStart() {
+//        super.onStart()
+//        val dialog = dialog as? BottomSheetDialog
+//        dialog?.findViewById<View>(R.id.cl_timer_bottom_sheet)?.let { bottomSheet ->
+//            val behavior = BottomSheetBehavior.from(bottomSheet)
+//            behavior.peekHeight = 100  // 시작 높이 설정
+//            behavior.isFitToContents = false // 컨텐츠 크기에 맞출지 여부
+//            behavior.state = BottomSheetBehavior.STATE_COLLAPSED // 기본적으로 접힌 상태
+//        }
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = BottomSheetTimerBinding.inflate(layoutInflater)
+
+        initNumberPicker()
+
+        initTimerButton()
+
+
+        return binding.root
+    }
+
+    private fun initTimerButton() {
+
+        binding.ivTimer30.setOnClickListener {
+            minute+=30
+            if(minute>59){
+                minute-=60
+                hour+=1
+            }
+            if(hour>24){
+                hour=0
+            }
+            binding.npHour.value = hour
+            binding.npMinute.value = minute
+        }
+        binding.ivTimer60.setOnClickListener {
+            hour+=1
+            if(hour>24){
+                hour=0
+            }
+            binding.npHour.value = hour
+        }
+        binding.ivTimer90.setOnClickListener {
+            minute+=30
+            hour+=1
+            if(minute>59){
+                minute-=60
+                hour+=1
+            }
+            if(hour>24){
+                hour=0
+            }
+            binding.npHour.value = hour
+            binding.npMinute.value = minute
+        }
+
+        binding.tvTimerReset.setOnClickListener {
+            binding.npHour.value = 0
+            binding.npMinute.value = 0
+            hour=0
+            minute=0
+        }
+
+        binding.tvTimerDone.setOnClickListener {
+            viewModel.sendData(hour,minute)
+            viewModel.closeDialog()
+            dismiss()
+        }
+
+    }
+
+    private fun initNumberPicker() {
+        val numberPickerHour = binding.npHour
+        val numberPickerMinute = binding.npMinute
+        numberPickerHour.minValue = 0
+        numberPickerHour.maxValue = 24
+        numberPickerMinute.minValue = 0
+        numberPickerMinute.maxValue = 59
+
+        numberPickerHour.setOnValueChangedListener { _, _, newVal ->
+            hour = newVal
+            Log.d("hour", hour.toString())
+        }
+
+        numberPickerMinute.setOnValueChangedListener{ _, _, newVal ->
+            minute = newVal
+            Log.d("minute", minute.toString())
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
