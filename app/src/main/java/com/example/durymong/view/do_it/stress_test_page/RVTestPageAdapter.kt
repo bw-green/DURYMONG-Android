fb1754cb564b09e7e85b9932edb42e8709bc99d0
bw-green <102360569+bw-green@users.noreply.github.com>
2025-01-31 09:42:35 +0900

    심리 검사 테스트 페이지 viewModel 과 선택할 수 있는거 3개 또는 4개라 해서 작업함

--- app/src/main/java/com/example/durymong/view/do_it/stress_test_page/RVTestPageAdapter.kt
+++ app/src/main/java/com/example/durymong/view/do_it/stress_test_page/RVTestPageAdapter.kt
@@ -5,22 +5,36 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.durymong.R
-import com.example.durymong.databinding.ItemTestBinding
 import com.example.durymong.databinding.ItemTestCompleteBinding
+import com.example.durymong.databinding.ItemTestFourBinding
+import com.example.durymong.databinding.ItemTestThreeBinding
 import com.example.durymong.view.do_it.stress_test_page.model.TestPageData
 
-class RVTestPageAdapter(private val itemList: List<TestPageData>, private val context: Context) :
+class RVTestPageAdapter(
+    private val itemList: LiveData<List<TestPageData>>,
+    private val context: Context
+) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
     companion object {
-        private const val VIEW_TYPE_NORMAL = 0
-        private const val VIEW_TYPE_LAST = 1
+        private const val VIEW_TYPE_FOUR = 0
+        private const val VIEW_TYPE_THREE = 1
+        private const val VIEW_TYPE_LAST = 2
+
     }
 
-    inner class ItemViewHolder(
-        private val binding: ItemTestBinding
+    inner class ThreeItemViewHolder(
+        private val binding: ItemTestThreeBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: TestPageData) {
+            binding.tvQuestionName.text = item.name
+        }
+    }
+
+    inner class FourItemViewHolder(
+        private val binding: ItemTestFourBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: TestPageData) {
             binding.tvQuestionName.text = item.name
@@ -38,36 +52,64 @@ class RVTestPageAdapter(private val itemList: List<TestPageData>, private val co
         }
     }
 
+
     override fun getItemViewType(position: Int): Int {
-        return if (position == itemList.size) {
+        return if (position == itemList.value?.size!!) {
             VIEW_TYPE_LAST
         } else {
-            VIEW_TYPE_NORMAL
+            if (itemList.value?.getOrNull(position)?.number == 4) {
+                VIEW_TYPE_FOUR
+            } else
+                VIEW_TYPE_THREE
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == VIEW_TYPE_LAST) {
-            val binding =
-                ItemTestCompleteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            LastItemViewHolder(binding)
-        } else {
-            val binding =
-                ItemTestBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            ItemViewHolder(binding)
+        return when (viewType) {
+            VIEW_TYPE_LAST -> {
+                val binding =
+                    ItemTestCompleteBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                LastItemViewHolder(binding)
+            }
+
+            VIEW_TYPE_THREE -> {
+                val binding =
+                    ItemTestThreeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ThreeItemViewHolder(binding)
+            }
+
+            else -> {
+                val binding =
+                    ItemTestFourBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                FourItemViewHolder(binding)
+            }
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is LastItemViewHolder) {
-            holder.bind()
-        } else if (holder is ItemViewHolder) {
-            holder.bind(itemList[position])
+        when (holder) {
+            is LastItemViewHolder -> {
+                holder.bind()
+            }
+
+            is ThreeItemViewHolder -> {
+                holder.bind(itemList.value?.getOrNull(position)!!)
+            }
+
+            else -> {
+                holder as FourItemViewHolder
+                holder.bind(itemList.value?.getOrNull(position)!!)
+            }
         }
     }
 
     override fun getItemCount(): Int {
-        return itemList.size + 1
+        if (itemList.value == null) return 0  // api 연결 오류로 인한 null
+        return itemList.value?.size!! + 1
     }
 
     fun showDialog(context: Context) {
@@ -81,6 +123,7 @@ class RVTestPageAdapter(private val itemList: List<TestPageData>, private val co
         okButton.setOnClickListener {
             dialog.dismiss()
         }
+
         dialog.show()
     }
 
--- app/src/main/java/com/example/durymong/view/do_it/stress_test_page/StressTestPageFragment.kt
+++ app/src/main/java/com/example/durymong/view/do_it/stress_test_page/StressTestPageFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.durymong.databinding.FragmentStressTestPageBinding
 import com.example.durymong.view.do_it.stress_test_page.model.TestPageData
+import com.example.durymong.view.do_it.stress_test_page.model.TestPageViewModel
 
 class StressTestPageFragment : Fragment() {
     private var _binding: FragmentStressTestPageBinding? = null
     private lateinit var stressRVTestPageAdapter: RVTestPageAdapter
-    private var dataList = ArrayList<TestPageData>()
 
     private val binding get() = _binding!!
+
+    private val viewModel: TestPageViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +25,6 @@ class StressTestPageFragment : Fragment() {
     ): View {
         _binding = FragmentStressTestPageBinding.inflate(layoutInflater)
 
-        initDummyData()
         initRvAdapter()
 
         return binding.root
@@ -33,40 +35,9 @@ class StressTestPageFragment : Fragment() {
         _binding = null
     }
 
-    private fun initDummyData() {
-        dataList.addAll(
-            arrayListOf(
-                TestPageData(
-                    "1. 쉽게 짜증이 나고 기분의 변동이 심하다."
-                ),
-                TestPageData(
-                    "2. 피부가 거칠고 각종 피부 질환이 심해졌다."
-                ),
-                TestPageData(
-                    "3. 온몸의 근육이 긴장되고 여기저기 쑤신다."
-                ),
-                TestPageData(
-                    "4. 수면 장애를 겪는다."
-                ),
-                TestPageData(
-                    "5. 매사에 자신이 없고 자기 비하를 많이 한다."
-                ),
-                TestPageData(
-                    "6. 별다른 이유 없이 불안 초조하다."
-                ),
-                TestPageData(
-                    "7. 쉽게 피로감을 느낀다."
-                ),
-                TestPageData(
-                    "8. 매사에 집중이 잘 안되고 일이나 학습 능률이 떨어진다."
-                )
-            )
-        )
-        binding.tvStressTestNumber.text = "${dataList.size} 문항"
-    }
-
     private fun initRvAdapter() {
-        stressRVTestPageAdapter = RVTestPageAdapter(dataList, requireContext())
+        stressRVTestPageAdapter = RVTestPageAdapter(viewModel.testPageList, requireContext())
+        binding.tvStressTestNumber.text = "${viewModel.testPageList.value?.size} 문항"
         binding.rvStressTest.apply {
             adapter = stressRVTestPageAdapter
             layoutManager = LinearLayoutManager(
