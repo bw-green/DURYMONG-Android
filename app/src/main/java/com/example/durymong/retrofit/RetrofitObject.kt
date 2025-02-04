b5aac11ee13fc85af79afdb9e06e671c4c3d9ed0
casperjr <hyunjw0515@gmail.com>
2025-02-05 05:07:06 +0900

    [feat]: RetrofitObject 작성

--- app/src/main/java/com/example/durymong/retrofit/RetrofitObject.kt
+++ app/src/main/java/com/example/durymong/retrofit/RetrofitObject.kt
@@ -1,8 +1,16 @@
 package com.example.durymong.retrofit
 
 import com.example.durymong.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitObject {
     private const val BASE_URL = BuildConfig.BASE_URL
 
+    val retrofit: Retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 }
