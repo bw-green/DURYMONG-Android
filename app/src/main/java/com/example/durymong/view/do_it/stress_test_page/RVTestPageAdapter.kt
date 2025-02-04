a3ffbec2ac9477b165e704dfa6261bcc77be5a7a
bw-green <102360569+bw-green@users.noreply.github.com>
2025-02-05 07:36:30 +0900

    수정된 테스트 화면 ui + 기능 (나가기는 구현안함)

--- app/src/main/java/com/example/durymong/view/do_it/stress_test_page/RVTestPageAdapter.kt
+++ app/src/main/java/com/example/durymong/view/do_it/stress_test_page/RVTestPageAdapter.kt
@@ -14,14 +14,13 @@ import com.example.durymong.databinding.ItemTestThreeBinding
 import com.example.durymong.view.do_it.stress_test_page.model.TestPageData
 
 class RVTestPageAdapter(
-    private val itemList: LiveData<List<TestPageData>>,
+    private val itemList: List<TestPageData>,
     private val context: Context
 ) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     companion object {
         private const val VIEW_TYPE_FOUR = 0
         private const val VIEW_TYPE_THREE = 1
-        private const val VIEW_TYPE_LAST = 2
 
     }
 
@@ -41,41 +40,15 @@ class RVTestPageAdapter(
         }
     }
 
-    inner class LastItemViewHolder(
-        private val binding: ItemTestCompleteBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
-            binding.ivTestComplete.setOnClickListener {
-                showDialog(context)
-            }
-
-        }
-    }
-
-
     override fun getItemViewType(position: Int): Int {
-        return if (position == itemList.value?.size!!) {
-            VIEW_TYPE_LAST
-        } else {
-            if (itemList.value?.getOrNull(position)?.number == 4) {
-                VIEW_TYPE_FOUR
-            } else
-                VIEW_TYPE_THREE
-        }
+        return if (itemList[position].number == 4) {
+            VIEW_TYPE_FOUR
+        } else
+            VIEW_TYPE_THREE
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
-            VIEW_TYPE_LAST -> {
-                val binding =
-                    ItemTestCompleteBinding.inflate(
-                        LayoutInflater.from(parent.context),
-                        parent,
-                        false
-                    )
-                LastItemViewHolder(binding)
-            }
-
             VIEW_TYPE_THREE -> {
                 val binding =
                     ItemTestThreeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -92,40 +65,19 @@ class RVTestPageAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
-            is LastItemViewHolder -> {
-                holder.bind()
-            }
-
             is ThreeItemViewHolder -> {
-                holder.bind(itemList.value?.getOrNull(position)!!)
+                holder.bind(itemList[position])
             }
 
             else -> {
                 holder as FourItemViewHolder
-                holder.bind(itemList.value?.getOrNull(position)!!)
+                holder.bind(itemList[position])
             }
         }
     }
 
     override fun getItemCount(): Int {
-        if (itemList.value == null) return 0  // api 연결 오류로 인한 null
-        return itemList.value?.size!! + 1
+        return itemList.size
     }
-
-    fun showDialog(context: Context) {
-
-        val dialog = Dialog(context)
-        dialog.setContentView(R.layout.dialog_test_result)
-        dialog.setCancelable(false)
-
-        val okButton = dialog.findViewById<ImageView>(R.id.iv_result_ok)
-
-        okButton.setOnClickListener {
-            dialog.dismiss()
-        }
-
-        dialog.show()
-    }
-
 
 }
