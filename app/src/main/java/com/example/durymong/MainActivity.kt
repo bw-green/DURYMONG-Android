17449c3e168ee249d43cff888b0c68e7361ed122
bw-green <102360569+bw-green@users.noreply.github.com>
2025-02-08 18:50:48 +0900

    [feat]: 상단바 안보이게 하기 수정필요하실 수정하면 될듯

--- app/src/main/java/com/example/durymong/MainActivity.kt
+++ app/src/main/java/com/example/durymong/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -18,7 +21,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        hideStatusBar()
+
         showBottomNavigation()
+    }
+
+    private fun hideStatusBar() {
+        // 1) Window fitsSystemWindows 설정 해제
+        WindowCompat.setDecorFitsSystemWindows(this.window, false)
+        // 2) WindowInsetsControllerCompat 사용
+        val insetsController = WindowInsetsControllerCompat(
+            this.window,
+            this.window.decorView
+        )
+        // 3) 상태바 바 숨기기
+        insetsController.hide(WindowInsetsCompat.Type.statusBars())
+
+        // 4) 스와이프로 시스템 바를 일시적으로 나타낼 수 있게 설정
+        insetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
     private fun showBottomNavigation() {
@@ -32,7 +53,7 @@ class MainActivity : AppCompatActivity() {
     private fun setBottomNavVisible(navController: NavController) {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             binding.bnvMain.visibility = when (destination.id) {
-                R.id.fragment_mong, R.id.fragment_chat, R.id.fragment_column, R.id.fragment_do_it, R.id.fragment_do_it_walk-> View.VISIBLE
+                R.id.fragment_mong, R.id.fragment_chat, R.id.fragment_column, R.id.fragment_do_it-> View.VISIBLE
                 else -> View.GONE
             }
         }
