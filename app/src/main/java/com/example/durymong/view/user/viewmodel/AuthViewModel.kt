09a9871c337b59f9f11bcbc6ef98410769f3ff08
casperjr <hyunjw0515@gmail.com>
2025-02-11 13:22:19 +0900

    [fix]: 토큰 저장오류 수정

--- app/src/main/java/com/example/durymong/view/user/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/durymong/view/user/viewmodel/AuthViewModel.kt
@@ -2,9 +2,11 @@ package com.example.durymong.view.user.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.durymong.model.TokenManager
 import com.example.durymong.model.dto.request.user.UserLoginRequestDto
 import com.example.durymong.model.repository.UserRepository
+import kotlinx.coroutines.launch
 
 class AuthViewModel : ViewModel() {
     private val repository = UserRepository()
@@ -15,17 +17,21 @@ class AuthViewModel : ViewModel() {
             password = "durymong12"
         )
 
-        repository.postLogin(userData) { response ->
-            if (response != null) {
-                Log.d("AuthViewModel", "로그인 성공: ${response.result.accessToken}")
-                val token = response.result.accessToken
-                if (token.isNotBlank()) {
-                     TokenManager.saveToken(token)
+        viewModelScope.launch {
+            repository.postLogin(userData) { response ->
+                if (response != null) {
+                    Log.d("AuthViewModel", "로그인 성공: ${response.result.accessToken}")
+                    val token = response.result.accessToken
+                    if (token.isNotBlank()) {
+                        TokenManager.saveToken(token)
+                        Log.d("AuthViewModel", "토큰 저장 성공 : $token")
+                    } else{
+                        Log.e("AuthViewModel", "토큰 저장 실패")
+                    }
+                } else {
+                    Log.e("AuthViewModel", "로그인 실패")
                 }
-            } else {
-                Log.e("AuthViewModel", "로그인 실패")
             }
         }
-
     }
 }
